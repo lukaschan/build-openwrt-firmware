6d7544efaf175d7cb5aa38e62bce4d16abaa939b
lukaschan <lorlno111@gmail.com>
2022-11-01 13:24:47 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -2,10 +2,10 @@
 
 (git clone https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
 
-cd openwrt
+#cd openwrt
 #echo "src-git udpxy https://github.com/haisongliang/udpxy.git;packages" >> feeds.conf.default
-echo "src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git xiaorouji2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+#echo "src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+#echo "src-git xiaorouji2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a

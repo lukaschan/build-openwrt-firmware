2ead76c57f569fe0dfc03039919861d1723f2b7e
lukaschan <lorlno111@gmail.com>
2022-11-04 14:35:51 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -3,7 +3,7 @@
 (git clone https://github.com/immortalwrt/immortalwrt openwrt  && git clone https://github.com/kiddin9/openwrt-packages openwrt) || (cd openwrt && git stash && git pull)
 
 cd openwrt
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;packages" >> feeds.conf.default
+echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
 echo "src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git xiaorouji2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 

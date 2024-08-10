ea82055b5194173e3043280756b2cc91431197e7
lukaschan <lorlno111@gmail.com>
2024-08-10 17:24:43 +0800

    Update x86-openwrt.sh

--- configs/x86-openwrt.sh
+++ configs/x86-openwrt.sh
@@ -38,7 +38,7 @@ make defconfig
 # 网络配置信息，将从 zzz-default-settings 文件的第2行开始添加
 sed -i "2i # network config" ./package/emortal/default-settings/files/99-default-settings
 # 默认 IP 地址，旁路由时不会和主路由的 192.168.1.1 冲突
-sed -i "3i uci set network.lan.ipaddr='192.168.2.252'" ./package/emortal/default-settings/files/99-default-settings
+sed -i "3i uci set network.lan.ipaddr='192.168.22.252'" ./package/emortal/default-settings/files/99-default-settings
 sed -i "4i uci set network.lan.proto='static'" ./package/emortal/default-settings/files/99-default-settings
 #sed -i "23d" ./packages/net/udpxy/Makefile
 #sed -i "11,16c PKG_VERSION:=1.0-25.1\PKG_RELEASE:=1\PKG_SOURCE_PROTO:=git\PKG_SOURCE_URL:=https://github.com/haisongliang/udpxy.git\PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)\PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION).tar.gz\PKG_MIRROR_HASH:=de19890237c99b8f9951e6d43e2b269ecaf44d16" ./packages/net/udpxy/Makefile
